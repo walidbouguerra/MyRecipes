469ef1665236bf3f28b8ca6335a721f888026774
Walid Bouguerra <160322152+walidbouguerra@users.noreply.github.com>
2025-01-05 20:49:51 +0100

    Add recipes methods show and index

---
+++ src/Controller/Recipe/RecipeController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller\Recipe;
+
+use App\Entity\Recipe;
+use App\Form\RecipeType;
+use App\Repository\RecipeRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Requirement\Requirement;
+
+#[Route('/recettes', name: 'recipe.')]
+class RecipeController extends AbstractController
+{
+    #[Route(name: 'index', methods: ['GET'])]
+    public function index(RecipeRepository $recipeRepository): Response
+    {
+        return $this->render('recipe/index.html.twig', [
+            'recipes' => $recipeRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/{slug}-{id}', name: 'show', requirements: ['slug' => Requirement::ASCII_SLUG ,'id' => Requirement::DIGITS], methods: ['GET'])]
+    public function show(Recipe $recipe): Response
+    {
+        return $this->render('recipe/show.html.twig', [
+            'recipe' => $recipe,
+        ]);
+    }
+}
